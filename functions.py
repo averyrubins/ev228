e299ae0ea03981a465f4739ef13f3f6bc7d8d117
averyrubins <a_rubins2023@coloradocollege.edu>
2025-11-03 10:55:08 -0700

    Updated functions.py
    refined and added new functions to import data, create plots and calculate descriptive statistics based on what we went over in class

--- functions.py
+++ functions.py
@@ -4,18 +4,36 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import scipy.stats as stats
 
-def xarray_import_data(file_path, file_name, column_index):
-    ds = xr.open_dataset(file_path + file_name)
-    da = ds[column_index]
-    print(da)
+'''functions to import data, create plots and calculate descriptive statistics'''
+
+def import_station(file_path='', var=''):
+    df = pd.read_csv(file_path)
+    df_data = df[var]
+    df_yr = df['YEAR']
+
+    return df_data, df_yr
+
+def import_gridded(file_path='', var=''):
+    ds = xr.open_dataset(file_path)
+    da = ds[var]
+
     return da
 
+def timeseries(in_df, in_x=None, out_path='', out_name=''):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+def map(in_da, out_path='', out_name=''):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    lons = in_da.longitude
+    lats = in_da.latitude
+
 def descriptive_stats(da):
-    dict_stat = {
-        'mean' : da.mean(),
-        'maximum' : da.max(),
-        'minimum' : da.min(),
-        'standard deviation' : da.std(),
-        'variance' : da.var()
-        }
-    return dict_stat
+    mean_var = np.mean(df)
+    stdev_var = np.std(df)
+    max_var = np.max(df)
+    min_var = np.min(df)
+    print(mean_var, stdev_var, max_var, min_var)
+
+return mean_var, stdev_var, max_var, min_var
