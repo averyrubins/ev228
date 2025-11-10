6ecdb49446bb44d25fc38163703451b293248a3b
averyrubins <a_rubins2023@coloradocollege.edu>
2025-11-10 11:39:32 -0700

    11/10 pt 1
    combined low and high vegetation datasets into one map

--- individual_vegtype.py
+++ individual_vegtype.py
@@ -14,16 +14,24 @@ import cartopy.feature as cfeature
 file_path = '/Users/averyrubins/data/ev228_data/era5_vegtype_2000-2025.nc'
 fig_path = '/Users/averyrubins/data/ev228_data/'
 fig_name = '2-era5_vegtype.png'
-data_var = 'tvl' 
+out_path = fig_path
+out_name = fig_name
+
 # tvh means type of high vegetation, and tvl means type of low vegetation
-da_vegtype = fn.import_gridded(file_path, var=data_var)
-da_vegtype_timemn = da_vegtype.mean(dim='valid_time')
+da1 = fn.import_gridded(file_path, var='tvl')
+da2 = fn.import_gridded(file_path, var='tvh')
+
+# combining high and low vegetation type data arrays
+var = da1 + da2
+
+# calculating time mean of vegetation type
+da_timemn = var.mean(dim='valid_time')
 
 # cartopy plot of vegetation type over time mean
 # code adapted from https://cartopy.readthedocs.io/stable/reference/projections.html#cartopy-projections with help from Theo
 fig = plt.figure(figsize=(8,8))
 ax = plt.axes(projection = ccrs.PlateCarree())
-da_vegtype_timemn.plot.pcolormesh(
+da_timemn.plot.pcolormesh(
     x = 'longitude',
     y = 'latitude',
     ax = ax,
@@ -47,6 +55,10 @@ ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
 ax.add_feature(cfeature.STATES, edgecolor='black', linewidth=0.5)
 ax.add_feature(cfeature.RIVERS, facecolor='blue', linewidth=1)
 
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.title('Vegetation Type in Southern Rocky Mountains, 2000-2025 mean')
+plt.savefig(out_path + out_name, dpi=400)
 plt.show()
 
 # need to write code to plot vegetation type from ERA5 data onto map, can use matplotlib website for instructions
